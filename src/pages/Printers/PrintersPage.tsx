34b724ec11900d6b14c3ed7df9c40ab35b33b8bc
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-03 21:39:23 +0200

    implemented printerTable component.

--- src/pages/Printers/PrintersPage.tsx
+++ src/pages/Printers/PrintersPage.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './PrintersPage.scss';
+import {Printer} from 'types';
+import {PrinterTable} from "../../components/PrinterTable/PrinterTable";
 
 export const PrintersPage = () => {
+  const [printers, setPrinters] = useState<null | Printer[]>(null)
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('http://localhost:3001/api/printers', {
+        method: 'GET',
+      });
+      const parsedList = await res.json();
+      setPrinters(parsedList.data)
+
+    })()
+  }, [])
+
+  if (printers === null) return <h1>loading...</h1>;
   return (
-    <h1>Printers Page</h1>
+    <>
+      <h1>Printers Page</h1>
+      <PrinterTable printers={printers}/>
+    </>
   )
 }
