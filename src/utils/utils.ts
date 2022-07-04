3bde3c699205aeb9323b0b5933f878f4eb91bf64
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-04 23:36:11 +0200

    added enum with printer models.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,10 @@
 export const isMultifunctionYesNoLabel = (param: boolean): string => {
   return param ? 'Yes' : 'No';
 }
+
+export enum PrinterModel {
+  Xerox_AltaLink_C8035 = 'Xerox AltaLink C8035',
+  Xerox_VersaLink_C605 = 'Xerox VersaLink C605',
+  Xerox_VersaLink_C400 = 'Xerox VersaLink C400',
+}
+
