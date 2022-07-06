465cfc017af527c8bd7f0d07dc6369705ae394a5
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-06 15:48:16 +0200

    moved routes to app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from './layout/Layout';
+import { Dashboard } from './pages/Dashboard/Dashboard';
+import { PrintersPage } from './pages/Printers/PrintersPage';
+import { ManagePrinterPage } from './pages/Printer/ManagePrinterPage';
+import { EditPrinterPage } from './pages/Printer/EditPrinterPage';
+import { AddPrinterPage } from './pages/Printer/AddPrinterPage';
+import { StorePage } from './pages/Store/StorePage';
+import { SuppliesPage } from './pages/Supplies/SuppliesPage';
+import { NotFoundPage } from './pages/error/NotFoundPage';
+import { PrintersContext } from './context/printers-context';
 
 function App() {
+  const [updatePrinters, setUpdatePrinters] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* eslint-disable-next-line react/jsx-no-constructed-context-values */}
+      <PrintersContext.Provider value={{ updatePrinters, setUpdatePrinters }}>
+        <BrowserRouter>
+          <Routes>
+            {/* eslint-disable-next-line react/no-children-prop */}
+            <Route path="/" element={<Layout children={<Dashboard />} />} />
+            {/* @todo add pagination to printers page */}
+            <Route path="printers" element={<Layout><PrintersPage /></Layout>} />
+            <Route path="printer">
+              <Route path="manage/:printerId" element={<Layout><ManagePrinterPage /></Layout>} />
+              <Route path="edit/:printerId" element={<Layout><EditPrinterPage /></Layout>} />
+              <Route path="add" element={<Layout><AddPrinterPage /></Layout>} />
+            </Route>
+            <Route path="store" element={<Layout><StorePage /></Layout>}>
+              <Route path="manage/:storeId" element={<Layout><StorePage /></Layout>} />
+            </Route>
+            <Route path="supplies" element={<Layout><SuppliesPage /></Layout>}>
+              <Route path="manage/:supplyId" element={<Layout><SuppliesPage /></Layout>} />
+            </Route>
+            <Route path="*" element={<Layout><NotFoundPage /></Layout>} />
+          </Routes>
+        </BrowserRouter>
+      </PrintersContext.Provider>
     </div>
   );
 }
