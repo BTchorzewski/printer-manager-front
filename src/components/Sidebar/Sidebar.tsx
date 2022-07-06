b122dc51107f3d40185115e54dbfc842817fc207
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-06 15:29:56 +0200

    styled Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Sidebar.scss';
 import { Link } from 'react-router-dom';
+import {
+  FiBox, FiHome, FiPrinter, FiTruck,
+} from 'react-icons/fi';
 
 export function Sidebar() {
   return (
@@ -10,10 +13,38 @@ export function Sidebar() {
       </header>
       <nav className="Sidebar__navigation">
         <ul className="Sidebar__list">
-          <li className="Sidebar__item"><Link className="Sidebar__link" to="/">Dashboard</Link></li>
-          <li className="Sidebar__item"><Link className="Sidebar__link" to="/printers">Printers</Link></li>
-          <li className="Sidebar__item"><Link className="Sidebar__link" to="/store">Store</Link></li>
-          <li className="Sidebar__item"><Link className="Sidebar__link" to="/supplies">Supplies</Link></li>
+          <li className="Sidebar__item">
+            <Link className="Sidebar__link" to="/">
+              <FiHome
+                className="Sidebar__icon"
+              />
+              Dashboard
+            </Link>
+          </li>
+          <li className="Sidebar__item">
+            <Link className="Sidebar__link" to="/printers">
+              <FiPrinter
+                className="Sidebar__icon"
+              />
+              Printers
+            </Link>
+          </li>
+          <li className="Sidebar__item">
+            <Link className="Sidebar__link" to="/store">
+              <FiTruck
+                className="Sidebar__icon"
+              />
+              Store
+            </Link>
+          </li>
+          <li className="Sidebar__item">
+            <Link className="Sidebar__link" to="/supplies">
+              <FiBox
+                className="Sidebar__icon"
+              />
+              Supplies
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
