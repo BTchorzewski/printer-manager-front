3f402fc7590ec128be8cbbf8f7179ae124e074f7
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-06 15:50:29 +0200

    added printer context that printer list is refreshed after removing printer.

--- src/pages/Printers/PrintersPage.tsx
+++ src/pages/Printers/PrintersPage.tsx
@@ -1,11 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './PrintersPage.scss';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Printer } from 'types';
 import { PrinterTable } from '../../components/PrinterTable/PrinterTable';
+import { PrintersContext } from '../../context/printers-context';
+import { BoxCenter } from '../../layout/BoxCenter';
 
 export function PrintersPage() {
   const [printers, setPrinters] = useState<null | Printer[]>(null);
+  const { updatePrinters } = useContext(PrintersContext);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('http://localhost:3001/api/printers', {
+        method: 'GET',
+      });
+      const parsedList = await res.json();
+      console.log(parsedList.data);
+      setPrinters(parsedList.data);
+    })();
+  }, []);
+
   useEffect(() => {
     (async () => {
       const res = await fetch('http://localhost:3001/api/printers', {
@@ -14,13 +28,12 @@ export function PrintersPage() {
       const parsedList = await res.json();
       setPrinters(parsedList.data);
     })();
-  }, []);
+  }, [updatePrinters]);
 
   if (printers === null) return <h1>loading...</h1>;
   return (
-    <>
-      <h1>Printers Page</h1>
-      <PrinterTable printers={printers} />
-    </>
+
+    <PrinterTable printers={printers} />
+
   );
 }
