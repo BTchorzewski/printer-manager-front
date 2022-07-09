f4dc17d31d5c7c09a380b7a882074816f600b5bc
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-09 13:29:52 +0200

    ended store page.

--- src/components/Store/StoreItem/StoreItem.tsx
+++ src/components/Store/StoreItem/StoreItem.tsx
@@ -1,16 +1,16 @@
-import React, { ChangeEvent, useContext, useState } from 'react';
+import React, {ChangeEvent, useContext, useState} from 'react';
 import './StoreItem.scss';
-import { SupplyAvailability } from '../../../types';
-import { StoreContext } from '../../../context/store-context';
+import {SupplyAvailability} from '../../../types';
+import {StoreContext} from '../../../context/store-context';
 
 interface Props {
   supply: SupplyAvailability
 }
 
-export function StoreItem({ supply }: Props) {
+export function StoreItem({supply}: Props) {
   const [quantity, setQuantity] = useState(1);
-  const { updateStore, setUpdateStore } = useContext(StoreContext);
-  const handleInput = (e:ChangeEvent<HTMLInputElement>) => {
+  const {updateStore, setUpdateStore} = useContext(StoreContext);
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setQuantity(parseInt(e.target.value, 10));
   };
 
@@ -24,29 +24,39 @@ export function StoreItem({ supply }: Props) {
         quantity,
       }),
     });
-    console.log(respond.status);
-    if (respond.status === 200) setUpdateStore(!updateStore);
+    const {msg} = await respond.json();
+
+    if (respond.status === 200) {
+      alert(msg);
+      setQuantity(1);
+      setUpdateStore(!updateStore);
+    } else {
+      alert(msg);
+    }
   };
 
   return (
-    <div key={supply.id} className="Supply__content">
-      <div className="Supply__info">
-        <p className="Supply__text">{supply.name}</p>
-        <p className="Supply__text">Total: {supply.total}</p>
-        <p className="Supply__text">Avaialable: {supply.totalAvailable}</p>
+    <div key={supply.id} className="StoreItem">
+      <div className="StoreItem__info">
+        <p className="StoreItem__text">{supply.name}</p>
+        <p className="StoreItem__text">
+          Total:
+          {supply.total}
+        </p>
+        <p className="StoreItem__text">
+          Available:
+          {supply.totalAvailable}
+        </p>
       </div>
-      <label htmlFor="quantity">
-        order:
-        <input
-          onChange={handleInput}
-          value={quantity}
-          type="number"
-          className="Supply__input"
-          min={1}
-          max={10}
-          id="quantity"
-        />
-      </label>
+      <input
+        onChange={handleInput}
+        value={quantity}
+        type="number"
+        className="StoreItem__input"
+        min={1}
+        max={10}
+        id="quantity"
+      />
       <button onClick={handleButton} type="button" className="StoreItem__button">add</button>
     </div>
   );
