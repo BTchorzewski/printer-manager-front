03e6d13b141386dd4afe7449408ea8d4a892ace5
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-11 21:19:25 +0200

    added title.

--- src/components/PrinterTable/PrinterTable.tsx
+++ src/components/PrinterTable/PrinterTable.tsx
@@ -13,30 +13,32 @@ interface Props {
 
 export function PrinterTable({ printers }: Props) {
   return (
-    <table className="Table">
-      <thead className="Table__head">
-        <th className="Table__head-item">Name</th>
-        <th className="Table__head-item">IP</th>
-        <th className="Table__head-item">Model</th>
-        <th className="Table__head-item">isMultifunctional</th>
-        <th className="Table__head-item">Area</th>
-        <th className="Table__head-item">Location</th>
-        <th className="Table__head-item">Actions</th>
-      </thead>
-      <tbody className="Table__body">
-        {
+    <div className="PrinterTable">
+      <h2 className="PrinterTable__title">Printers</h2>
+      <table className="PrinterTable__table">
+        <thead className="PrinterTable__head">
+          <th className="PrinterTable__head-item">Name</th>
+          <th className="PrinterTable__head-item">IP</th>
+          <th className="PrinterTable__head-item">Model</th>
+          <th className="PrinterTable__head-item">isMultifunctional</th>
+          <th className="PrinterTable__head-item">Area</th>
+          <th className="PrinterTable__head-item">Location</th>
+          <th className="PrinterTable__head-item">Actions</th>
+        </thead>
+        <tbody className="PrinterTable__body">
+          {
         printers.map((printer) => (
           <tr
-            className="Table__row"
+            className="PrinterTable__row"
             key={printer.id}
           >
-            <td className="Table__data">{printer.name}</td>
-            <td className="Table__data">{printer.ip}</td>
-            <td className="Table__data">{printer.model}</td>
-            <td className="Table__data">{isMultifunctionYesNoLabel(printer.isMultifunctional)}</td>
-            <td className="Table__data">{printer.area}</td>
-            <td className="Table__data">{printer.location}</td>
-            <td className="Table__actions">
+            <td className="PrinterTable__data">{printer.name}</td>
+            <td className="PrinterTable__data">{printer.ip}</td>
+            <td className="PrinterTable__data">{printer.model}</td>
+            <td className="PrinterTable__data">{isMultifunctionYesNoLabel(printer.isMultifunctional)}</td>
+            <td className="PrinterTable__data">{printer.area}</td>
+            <td className="PrinterTable__data">{printer.location}</td>
+            <td className="PrinterTable__actions">
               <EditPrinterButton printerId={printer.id}>Edit</EditPrinterButton>
               <ManagePrinterButton printerId={printer.id}>Manage</ManagePrinterButton>
               <DeletePrinterButton printerId={printer.id}>Delete</DeletePrinterButton>
@@ -45,7 +47,8 @@ export function PrinterTable({ printers }: Props) {
           </tr>
         ))
       }
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
